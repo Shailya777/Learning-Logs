bfe5f72e376036f70c893ae949d32e73c905768b
Shailya Gandhi <shailya.gandhi100@gmail.com>
2025-08-02 18:28:44 +0530

    Deleted unnecessary folder.

--- 1) Python Data Types/Python/1) Overview.py
+++
@@ -1,9 +0,0 @@
-# Python Data Types;
-
-# 1) Numeric:  int, float, complex
-# 2) Text: str
-# 3) Sequence: list, tuple, range
-# 4) Mapping: dict
-# 5) Boolean: bool
-# 6) Binary: bytes, bytearray, memoryview
-# 7) Set: set, frozenset
